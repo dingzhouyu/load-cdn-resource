b29167d17769912b0e5f1bba5a0ebdabbbacb0bc
丁周裕 <dingzhouyu@ones.cn>
2022-08-22 09:43:31 +0800

    feat: add ssg support

---
+++ pages/auth/join_team/index.tsx
@@ -0,0 +1,11 @@
+export interface IJoinTeamProps {
+
+}
+
+export default function JoinTeam(props: IJoinTeamProps) {
+  return (
+    <>
+      <h2>hello world</h2>
+    </>
+  )
+}
---
+++ pages/auth/login/index.tsx
@@ -0,0 +1,9 @@
+export interface ILoginProps {}
+
+export default function Login(props: ILoginProps) {
+  return (
+    <>
+      <h2>hello Login</h2>
+    </>
+  )
+}
